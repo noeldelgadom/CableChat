1f23a4988739b9cd95d22b8c0d9639b8d3bb4bc4
Noel Delgado <noeldelgadom@myhost.local>
2017-08-15 10:49:32 -0500

    missed saving the message_broad_cast_job.rb file on last commit

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -12,20 +12,3 @@ class MessageBroadcastJob < ApplicationJob
     MessagesController.render partial: 'messages/message', locals: {message: message}
   end
 end
-
-
-
-class MessageBroadcastJob < ApplicationJob
-  queue_as :default
-
-  def perform(message)
-    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
-                                 message: render_message(message)
-  end
-
-  private
-
-  def render_message(message)
-    MessagesController.render partial: 'messages/message', locals: {message: message}
-  end
-end
