cda087aac0136c5d6eb50c87cd3c2a6c03684df6
Noel Delgado <noeldelgadom@myhost.local>
2017-08-15 09:25:23 -0500

    Messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'chat_rooms/index'
-
-  get 'chat_rooms/new'
-
   devise_for :users
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
